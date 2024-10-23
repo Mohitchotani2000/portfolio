b11780562ba8c6ba7c5ea3d056e1170988605ce6
Mohitchotani2000 <mohitchotani2000@gmail.com>
2024-10-23 23:30:53 +0530

    animation added

--- js/scripts.js
+++ js/scripts.js
@@ -98,3 +98,36 @@
     });
 
 })(jQuery);
+
+
+const dynamicText = document.getElementById('dynamic-text');
+const texts = ['Software Engineer', 'Fullstack Developer'];
+let textIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function type() {
+  const currentText = texts[textIndex];
+  dynamicText.innerText = currentText.substring(0, charIndex);
+
+  let speed = isDeleting ? 100 : 150;
+
+  if (!isDeleting && charIndex === currentText.length) {
+    speed = 1000;
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    textIndex = (textIndex + 1) % texts.length;
+    speed = 500; 
+  }
+
+  if (isDeleting) {
+    charIndex--; 
+  } else {
+    charIndex++;
+  }
+
+  setTimeout(type, speed);
+}
+
+document.addEventListener('DOMContentLoaded', () => type());
